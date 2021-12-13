aed6798f275f6e19ce494fc06c23ef8373b55227
Gustav Sörnäs <gustav@sornas.net>
2021-12-13 19:19:53 +0100

    good code

--- star-star.c
+++ star-star.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
-int main(){int i,j,x;int first_part[]={9,1,8,3,7,5,6,7,0,19,2,15,4,12};int second_part[]={4,5,1,5,3,4,5,4,2,3,9,3};for(i=0;i<sizeof(first_part)/sizeof(first_part[0]);i+=2){for(x=0;x<first_part[i];++x){printf(" ");}for(x=0;x<first_part[i+1];++x){printf("*");}printf("\n");}for(i=0;i<sizeof(second_part)/sizeof(second_part[0]);i+=4){for(x=0;x<second_part[i];++x){printf(" ");}for(x=0;x<second_part[i+1];++x){printf("*");}for(x=0;x<second_part[i+2];++x){printf(" ");}for(x=0;x<second_part[i+3];++x){printf("*");}printf("\n");}}
+#define p printf
+
+                    int
+                   main()
+                 {int i,j,x;
+                int a[]={9,1,
+               8,3,7,5,6,7,0,19
+             ,2,15,4,12 };int b[]
+            ={4,5,1,5,3,4,5,4,2,3,9
+,3};for(i=0;i< sizeof(a)/sizeof(a[0]);i+= 2){for
+  (x=0;x< a[i];++x){p(" ");}for(x=0;x<a[i+1];
+    ++x){p("*");}p("\n");}for(i=0;i<sizeof
+     (b)/sizeof(b[0]);i+=4){for(x=0;x<b[
+       i];++x){p(" ")    ;}for(x=0;x<b[i
+      +1];++x){p(           "*");}for(x=0;
+     x<b[i+2];                  ++x){p(" "
+    );}for(x                     =0;x<b[i+
+   3];++x){                        p("*");
+  }p(""""                           "\n");}}
